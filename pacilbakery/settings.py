14646909a701f64afef92a42b1b152ff09aee0ae
irfanrizqinr <irfanrizqi2005@gmail.com>
2024-11-20 03:22:25 +0700

    Tugas 9 Selesai

--- pacilbakery/settings.py
+++ pacilbakery/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-u%q*u7xd5tn3%$0_g6=&p(46dgsn$fur1s9+@vgm$661re+3n8
 PRODUCTION = os.getenv("PRODUCTION", False)
 DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1","irfan-rizqi31-pacilbakery.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","irfan-rizqi31-pacilbakery.pbp.cs.ui.ac.id", "10.0.2.2"]
 
 
 # Application definition
@@ -28,6 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'authentication',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -39,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'pacilbakery.urls'
@@ -103,6 +106,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
